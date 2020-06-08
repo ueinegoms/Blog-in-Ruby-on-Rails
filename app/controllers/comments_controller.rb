c526fa9d00fbc21d392b462d67609055a0d8ef63
Wayne Gomes <waynegomes.wg@gmail.com>
2020-06-08 11:10:54 -0300

    Posts complete, Comments Complete and Basic Authentication as well

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+    http_basic_authenticate_with name: 'testeiro', password: '123', only: [:destroy]
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comments_params)
+        redirect_to post_path(@post)
+    end
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
+    end
+    private def comments_params
+        params.require(:comment).permit(:username, :body)
+    end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: 'testeiro', password: '123', except: [:index, :show]
+
     def index
         @posts = Post.all
     end
@@ -6,7 +8,7 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
     def new
-
+        @post = Post.new
     end
     def create
         #how to debug a form submit
@@ -14,10 +16,29 @@ class PostsController < ApplicationController
 
         @post = Post.new(post_params)
 
-        @post.save
-        redirect_to @post
+        if(@post.save)
+            redirect_to @post
+        else 
+            render 'new'
+        end
     end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])  
+        if(@post.update(post_params))
+            redirect_to @post
+        else 
+            render 'edit'
+        end
+    end
+    def destroy
+        @post = Post.find(params[:id])  
+        @post.destroy
 
+        redirect_to posts_path
+    end
     private def post_params
         params.require(:post).permit(:title, :body)
     end
